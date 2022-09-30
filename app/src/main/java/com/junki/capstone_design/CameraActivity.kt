a634f3b4010f0dacc4106f4032fa19907de72d76
junki <ghdwnsrl1991@gmail.com>
2022-09-30 13:47:40 +0900

    Camera preview 추가

--- app/src/main/java/com/junki/capstone_design/CameraActivity.kt
+++ app/src/main/java/com/junki/capstone_design/CameraActivity.kt
@@ -2,10 +2,54 @@ package com.junki.capstone_design
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+import com.junki.capstone_design.databinding.ActivityCameraBinding
+import com.junki.capstone_design.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_camera.*
+
 
 class CameraActivity : AppCompatActivity() {
+
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        startCamera()
+
     }
+
+
+    fun startCamera(){
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+
+    }
+
+    fun bindPreview(cameraProvider : ProcessCameraProvider) {
+        var preview : Preview = Preview.Builder()
+            .build()
+
+        var cameraSelector : CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider())
+
+        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+    }
+
+
+
 }
